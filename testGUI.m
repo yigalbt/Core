d22bbe799bfc7572c3f4c6b209453dd4a8bd70d5
Yigal Ben Tal <yigal@bondit.co.il>
2012-12-30 12:26:04 +0200

    Test commit.
    Added new 2 functions.

---
+++ testGUI.m
@@ -0,0 +1,107 @@
+function varargout = testGUI(varargin)
+% TESTGUI MATLAB code for testGUI.fig
+%      TESTGUI, by itself, creates a new TESTGUI or raises the existing
+%      singleton*.
+%
+%      H = TESTGUI returns the handle to a new TESTGUI or the handle to
+%      the existing singleton*.
+%
+%      TESTGUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in TESTGUI.M with the given input arguments.
+%
+%      TESTGUI('Property','Value',...) creates a new TESTGUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before testGUI_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to testGUI_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help testGUI
+
+% Last Modified by GUIDE v2.5 13-Nov-2012 22:31:35
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @testGUI_OpeningFcn, ...
+                   'gui_OutputFcn',  @testGUI_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before testGUI is made visible.
+function testGUI_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to testGUI (see VARARGIN)
+
+
+% Commet/uncomment the next 6 line to disable/enable tabs
+hTabGroup = uitabgroup('Parent',gcf);
+hTabs(1) = uitab('Parent',hTabGroup, 'Title','Data');
+hTabs(2) = uitab('Parent',hTabGroup, 'Title','Params');
+set(hTabGroup, 'SelectedTab',hTabs(1));
+set(handles.panelA,'Parent',hTabs(1))
+set(handles.panelB,'Parent',hTabs(2))
+
+
+
+set(handles.output,'String',num2str(1))
+
+
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes testGUI wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = testGUI_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.output,'String','abc')
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.output,'string',num2str(2))
+
+% --- Executes during object creation, after setting all properties.
+function panelA_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to panelA (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
